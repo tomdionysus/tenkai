71bb44fa44d97679d0bf9c73ffaa9e3f9b663536
Tom Cully <tom@blackraven.co.nz>
2020-04-11 21:45:13 +1200

    Stub test

--- spec/Entity_spec.js
+++ spec/Entity_spec.js
@@ -1,2 +1,10 @@
 const Entity = require("../lib/Entity")
 
+describe('Entity', () => {
+	it('should allow New', () => {
+		var x1 = new Entity()
+		var x2 = new Entity()
+
+		expect(x1).not.toBe(x2)
+	})
+})
